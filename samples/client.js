1a629b514dbea5e9f99afe2a1724b41400e0ffc6
Alex Hoyau <a.hoyau@silexlabs.org>
2019-10-19 14:58:16 -0300

    work on samples

--- samples/client.js
+++ samples/client.js
@@ -1,9 +1,24 @@
 // in samples/cli.js
-import {read} from '../lib/store.js'
+import {write, read} from '../lib/store.js'
+import {aggregate} from '../lib/aggregate.js'
 const EXAMPLE_ID = 'a-random-ID'
 
 setInterval(() => {
   read(EXAMPLE_ID, new Date())
+  .then(state => aggregate(state, new Date()))
   .then(state => console.log('State', JSON.stringify(state)))
 }, 1000)
+
+// this should be in another thread, and should be blocking if we read / aggregate / write
+// setInterval(() => {
+//   read(EXAMPLE_ID)
+//   .then(data => {
+//     if(data) {
+//       const aggregated = aggregate(data, new Date())
+//       write(EXAMPLE_ID, aggregated)
+//       console.log('aggregated:', JSON.stringify(aggregated))
+//     }
+//   })
+// }, 100)
+
 
--- samples/server.js
+++ samples/server.js
@@ -1,6 +1,5 @@
 // in samples/server.js
 import {aggregate} from '../lib/aggregate.js'
-import {write, read} from '../lib/store.js'
 import {hit} from '../lib/utils.js'
 
 const EXAMPLE_STATES = [{name: 'first'}, {name: 'second'}, {name: 'third'}]
@@ -8,26 +7,17 @@ const EXAMPLE_ID = 'a-random-ID'
 
 function randomHit() {
   setTimeout(() => {
-    // write(EXAMPLE_ID, {
-    //   state: EXAMPLE_STATES[Math.floor(Math.random() * EXAMPLE_STATES.length)],
-    // }, new Date())
-    // .then(record => console.log('written:', record))
-    const result = hit(EXAMPLE_ID, new Date(), {
+    const data = {
       state: [EXAMPLE_STATES[Math.floor(Math.random() * EXAMPLE_STATES.length)]],
       num: 1,
+    }
+    console.log('Fake hit', data)
+    hit(EXAMPLE_ID, new Date(), data)
+    .then(result => {
+      // keep hitting
+      randomHit()
     })
-    console.log('HIT', EXAMPLE_STATES, result)
-    randomHit()
   }, Math.random() * 10000)
 }
 randomHit()
-
-// this should be in another thread
-setInterval(() => {
-  read(EXAMPLE_ID)
-  .then(data => {
-    const aggregated = aggregate(data, new Date())
-    console.log('aggregated:', JSON.stringify(aggregated))
-  })
-}, 100)
 
